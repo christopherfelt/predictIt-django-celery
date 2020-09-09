711b4b6637777dfb9387f8e5148792ce4549a67d
chrisfelt <chrisfelt.webdev@gmail.com>
2020-09-08 20:59:16 -0600

    added notes

--- project/celery.py
+++ project/celery.py
@@ -8,3 +8,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+# celery return module not found
